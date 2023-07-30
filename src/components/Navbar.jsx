aa055ca281c07db21c6b6bdd2224020120b717b9
Alcaraz <jaime.mediano@gmail.com>
2023-07-30 17:25:22 +0200

    No idea whats wrong or how to fix it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,19 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <ul style={{ listStyle: "none" }}>
+          <li>
+            <Link to="/">
+              <h1>Wiki Countries</h1>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,15 @@
-function HomePage() {}
+import { useState } from "react";
+import axios from "axios";
+
+async function HomePage() {
+  const response = await axios.get(
+    `https://ih-countries-api.herokuapp.com/countries`
+  );
+  const [countries, SetCountries] = useState(response);
+
+  console.log(response);
+
+  return <h1>WikiCountries: Your Guide to the World</h1>;
+}
 
 export default HomePage;
